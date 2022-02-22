43498087ab0c89d7246ea995a93fbc7f6d74b87b
Perica Rajčević <perica.rajcevic@fpmoz.sum.ba>
2022-02-22 17:59:54 +0100

    Dashboard fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+                'email' => ['Unešeni podaci nisu ispravni.'],
             ]);
         }
 
